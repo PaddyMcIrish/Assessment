538d6ee87a0e462109760e380e0fd3a8c773686c
PaddyMcIrish <66040870+PaddyMcIrish@users.noreply.github.com>
2020-08-24 12:17:24 +1200

    Add files via upload

--- SchoolAssement.v2cc/Dialema.java
+++ SchoolAssement.v2cc/Dialema.java
@@ -4,21 +4,25 @@ import java.util.Scanner;//Keyboard reading.
  *
  * @author (Virgil)
  * @version (v2)
- * v1 added Scoreing Method 
- * v2 added Array for Com to decide what to do
- * Testing  github
+ * 7/6/20 v1 added Scoring Method .
+ * 14/6/20 v2 added Array for Com to decide what to do.
+ * 17/6/20 Testing  github.
+ * 29/6/20 Starting on computers method to decide it's move.
+ * 20/7/20 Made Progress on Method.
+ * 22/7/20 Currently computer changes to "c" on round 3 (works but not as i want it to).
  */
 public class Dialema
 {
-    // instance variables - replace the example below with your own
+    // The variables that the code uses.
     private int r;
-    private int p;
-    private int Com1;
     private int y;
     private int pScore;
     private int cScore;
     private int pTotal;
     private int cTotal;
+    private int cs; 
+    private int hr;
+    private int x;
     private String pChoice;
     private String cChoice;
     /**
@@ -27,23 +31,44 @@ public class Dialema
     public Dialema()//the main loop
     {
         Scanner inputStream = new Scanner(System.in);
-        Scanner cInputString = new Scanner(System.in);
-        
+        //Scanner cInputString = new Scanner(System.in);
         System.out.println("press c to cooperate");
         System.out.println("press d to defect");
         String choice;
         cChoice = ("c");
-        int x = 0;
-        String pMoves[] = new String[4];
-        for(r=0;r<4;r++){
+        x = 1;
+        y = 1;
+        cs = 0;
+        String pMoves[] = new String[10];
+        String cMoves[] = new String[10];
+        for(r=1;r<10;r++){
             pChoice = inputStream.nextLine();
-            if (pChoice.equals("c")){//this array stores the players moves for the com to access
+            if(r>2){
+                if(pMoves[x-1].equals("d")){
+                    cChoice=("d");
+                } else if (cMoves[y-1].equals("d")) {
+                    cChoice=("d");
+                } else if (r>5) {
+                    cChoice=("d");
+                } else {
+                    cChoice=("c");
+                }
+            }
+            if (pChoice.equals("c")){ //this array stores the players moves for the com to access
                 pMoves[x]=("c");
                 x++;
             }  
             else if(pChoice.equals("d")){
                 pMoves[x]=("d");
                 x++;
+            }
+            if (cChoice.equals("c")){ //this array stores the players moves for the com to access
+                cMoves[y]=("c");
+                y++;
+            }  
+            else if(cChoice.equals("d")){
+                cMoves[y]=("d");
+                y++;
             }
             System.out.println("Player Score " + pScore(pChoice,cChoice));
             System.out.println("Computer Score " + cScore(pChoice,cChoice));    
@@ -52,9 +77,9 @@ public class Dialema
             System.out.println("Players Total Score " + pTotal);
             System.out.println("Coms Total Score " + cTotal);
         }
-        r = 0;
     }
-    private int pScore(String pPlayed,String cPlayed) //should change the change the players score
+
+    private int pScore(String pPlayed,String cPlayed) //manages the players score
     {
         if(cChoice.equals("c")){
             if(pChoice.equals("c")){
@@ -75,8 +100,9 @@ public class Dialema
         else{
             return 0;
         }
-    }
-    private int cScore(String pPlayed,String cPlayed) //should change the change the players score
+    }   
+
+    private int cScore(String pPlayed,String cPlayed) //manages the computers score
     {
         if(pChoice.equals("c")){
             if(cChoice.equals("c")){
